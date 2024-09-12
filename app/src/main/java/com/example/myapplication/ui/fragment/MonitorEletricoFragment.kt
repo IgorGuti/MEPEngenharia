04e14238128906b21aec612163f876a33367d3b6
Igor Gutierrez <iguticarlos95@gmail.com>
2024-09-12 11:22:28 -0300

    Tela de monitor eletrico e reservatorio sem erros

--- app/src/main/java/com/example/myapplication/ui/fragment/MonitorEletricoFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/fragment/MonitorEletricoFragment.kt
@@ -52,6 +52,7 @@ class MonitorEletricoFragment : Fragment() {
                 // Implementar lógica de clique aqui, se necessário
             }, apiRepository)
             recyclerViewMonitorEletrico.adapter = adapter
+            //resolvido
         }
     }
 }
